d4e2c9269012be7a6edde5e9df7514a3743bb04e
Aniket Patel <111775702+Aniket-Patel-swg@users.noreply.github.com>
2022-10-15 20:06:30 +0530

    Add files via upload

--- Client/src/App.js
+++ Client/src/App.js
@@ -6,17 +6,22 @@ import Messages from './Pages/Messages';
 import Charts from './Pages/Charts';
 import ProductInfo from './Elements/productInfo';
 import DeadStock from './Pages/DeadStock';
+import HomePage from './Pages/HomePage';
+import AboutUs from './Pages/AboutUs';
+
 
 function App() {
   return (
    <BrowserRouter>
     <Routes>
-      <Route path='/' element={ <Dashboard key = "dashboard" /> } />
+      <Route path='/Dashboard' element={ <Dashboard key = "dashboard" /> } />
       <Route path='/History' element = { <Histroy key="history" />} />
       <Route path='/Messages' element = {<Messages key="message" />}/>
       <Route path='/Charts' element = { <Charts key="charts" /> }/>
       <Route path='/ProductInfo' element = {<ProductInfo key="ProductInfo" />} />
       <Route path='/DeadStock' element = {<DeadStock />}/>
+      <Route path='/' element = { <HomePage /> }/>
+      <Route path='/about' element = {<AboutUs />}/>
      </Routes>
    </BrowserRouter>
   );
